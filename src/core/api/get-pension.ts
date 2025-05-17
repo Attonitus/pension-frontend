a477a4a564c091b245609d56d6b9717c52809f7f
Jesus Antonio <valenzuelarodriguezj512@gmail.com>
2025-05-16 20:56:39 -0600

    alpha 1.1

--- src/core/api/get-pension.ts
+++ src/core/api/get-pension.ts
@@ -16,7 +16,7 @@ interface IData {
 
 export const getPension = async(dataToSend: IData) => {
     try {
-        const res = await fetch("http://localhost:3000/api/calculator/complete", {
+        const res = await fetch("https://pension-calculator-xoak.onrender.com/api/calculator/complete", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
